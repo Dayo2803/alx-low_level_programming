dcb358f36d8bdfada55b2c5b207744123a4a47c4
Dayo2803 <folushooyinkansola@gmail.com>
2023-08-17 15:39:10 -0700

    isupper

--- 0-isupper.c/0-isupper.c
+++ 0-isupper.c/0-isupper.c
@@ -4,12 +4,12 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: 0
+ * Return: Always 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
