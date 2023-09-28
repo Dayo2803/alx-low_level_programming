073766c8e5a6c0715ca517a16f1cdecde181b91c
Dayo2803 <folushooyinkansola@gmail.com>
2023-09-28 12:38:54 -0700

    getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of bit at an index in decimal number
+ * @n: number to search
+ * @index: index of bit
+ *
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int dune;
+
+	if (index > 63)
+		return (-1);
+
+	dune = (n >> index) & 1;
+
+	return (dune);
+}
