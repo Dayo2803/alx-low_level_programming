97f404d4e6251c56d65fc93574d0db2f5234e384
Dayo <folushooyinkansola@gmail.com>
2023-12-11 04:42:52 -0800

    last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,4 @@
-wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+#!/bin/bash
+#wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+wget -p /tmp https://github.com/Dayo2803/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
 export LD_PRELOAD=/tmp/iwin.so
