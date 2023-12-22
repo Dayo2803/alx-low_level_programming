39a67ed597de1f2c20848fe9560ee49a6e7eaf8b
Dayo <folushooyinkansola@gmail.com>
2023-12-22 01:32:48 -0800

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	hash_table_t *hash_table_item = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	if (!hash_table)
+	if (!hash_table_item)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-	if (!hash_table->array)
+	hash_table_item->size = size;
+	hash_table_item->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (!hash_table_item->array)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+		hash_table_item->array[i] = NULL;
+	return (hash_table_item);
 }
